a1fb2ad1926af8255bbc385b91147c78ab8cb09f
Juan Ribeiro <juan.ribeiro3@outlook.com>
2023-01-12 18:10:57 -0300

    Co-authored-by: Thiago Perim <tperim@gmail.com>

---
+++ src/pages/Game.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../components/header';
+
+class Game extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+export default Game;
