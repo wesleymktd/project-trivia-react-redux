e6f1f1a878a4ff2f5ac7f2505ad31af4b05f1061
Juan Ribeiro <juan.ribeiro3@outlook.com>
2023-01-16 18:05:23 -0300

    Alteracoes feitas no req 8

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Ranking extends Component {
+  state = {
+    doRedirectLogin: false,
+  };
+
+  setGoHome = () => {
+    this.setState({
+      doRedirectLogin: true,
+    });
+  };
+
   render() {
+    const { doRedirectLogin } = this.state;
     return (
-      <div>Ranking</div>
+      <div>
+        <h3 data-testid="ranking-title">Ranking Geral</h3>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.setGoHome }
+        >
+          Play Again
+        </button>
+        { doRedirectLogin && <Redirect to="/" /> }
+      </div>
     );
   }
 }
